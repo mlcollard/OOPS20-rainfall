8085ee00a4fd750b2afcd9cda87e792b2d9d8b3a
Michael L. Collard <collard@uakron.edu>
2020-01-13 08:54:49 -0500

    Add initial files for working project

---
+++ rainfall.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<float> rainfall;
+    float t;
+    float m;
+    float n;
+    while (cin >> n) {
+        rainfall.push_back(n);
+    }
+    t = rainfall[0];
+    m = rainfall[0];
+    for (int i = 1; i < rainfall.size(); ++i) {
+        t += rainfall[i];
+        if (rainfall[i] > m)
+            m = rainfall[i];
+    }
+    cout << "Average Hourly Rainfall: " << (t / rainfall.size()) << " hundreds of inches" << '\n';
+    cout << "Heaviest Hourly Rainfall: " << m << " hundreds of inches" << '\n';
+}
