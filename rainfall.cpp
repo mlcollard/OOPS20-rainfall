c7b519c9d42b8b131824aae0a553c4bf48bd7dfd
George <georgej1088@gmail.com>
2020-01-21 14:16:56 -0500

    Inline function definitions (they aren't useful)

--- rainfall.cpp
+++ rainfall.cpp
@@ -9,28 +9,16 @@
 #include <iterator>
 #include <numeric>
 
-/**
- * Finds the maximum value from a list of numbers.
- * @param data Container of data to find the maximum of
- */
-double maximum(const std::vector<double>& data) {
-    return *std::max_element(data.begin(), data.end());
-}
-
-/**
- * Calcuates the arithmetic mean of some data.
- * @param data Container of data to calculate the mean of
- */
-double mean(const std::vector<double>& data) {
-    return std::accumulate(data.begin(), data.end(), 0.0) / data.size();
-}
-
 int main() {
 
     // Get rainfall data from stdin
     std::vector<double> rainfall {std::istream_iterator<double>(std::cin), std::istream_iterator<double>()};
 
+    // Calculate necessary statistics
+    double mean = std::accumulate(rainfall.begin(), rainfall.end(), 0.0) / rainfall.size();
+    double maximum = *std::max_element(rainfall.begin(), rainfall.end());
+
     // Generate the report and send to stdout
-    std::cout << "Average Hourly Rainfall: " << mean(rainfall) << " hundreds of inches" << '\n';
-    std::cout << "Heaviest Hourly Rainfall: " << maximum(rainfall) << " hundreds of inches" << '\n';
+    std::cout << "Average Hourly Rainfall: " << mean << " hundreds of inches" << '\n';
+    std::cout << "Heaviest Hourly Rainfall: " << maximum << " hundreds of inches" << '\n';
 }
