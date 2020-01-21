93c144989ca0cf0f757ce13d9d0aaf048c7c1b07
PlanovskyJus <j.planovsky@gmail.com>
2020-01-21 13:40:01 -0500

    Extract function for heaveist rainfall

--- rainfall.cpp
+++ rainfall.cpp
@@ -6,28 +6,39 @@
 #include <iostream>
 #include <vector>
 
+// maximum data in the vector
+double heaviest(const std::vector<double>& data) {
+
+    auto max = data[0];
+    for (int i = 1; i < data.size(); ++i) {
+        if (data[i] > max)
+            max = data[i];
+    }
+    return 0;
+}
+
 int main() {
     // input hourly rainfall data
-    vector<double> rainfall;
+    std::vector<double> rainfall;
     double n;
     while (std::cin >> n) {
         rainfall.push_back(n);
     }
 
     //calculate average and heaviest rainfall
-    double max = rainfall[0];
+    auto max = heaviest(rainfall);
     for (int i = 1; i < rainfall.size(); ++i) {
         if (rainfall[i] > max)
             max = rainfall[i];
     }
-    
-    double total = rainfall[0];
+
+    auto total = rainfall[0];
     for (int i = 1; i < rainfall.size(); ++i) {
         total += rainfall[i];
     }
-    double average = total / rainfall.size();
+    auto average = total / rainfall.size();
 
     // output rainfall report
-    cout << "Average Hourly Rainfall: " << average << " hundreds of inches" << '\n';
-    cout << "Heaviest Hourly Rainfall: " << max << " hundreds of inches" << '\n';
+    std::cout << "Average Hourly Rainfall: " << average << " hundreds of inches" << '\n';
+    std::cout << "Heaviest Hourly Rainfall: " << max << " hundreds of inches" << '\n';
 }
